44d3a19c2f6feca8668e6d8f30beb83cbb7d86bc
santhx1 <136553429+santhx1@users.noreply.github.com>
2024-02-08 16:43:17 -0300

    futebol

--- index.js
+++ index.js
@@ -1,91 +1,91 @@
 const perguntas = [
   {
-    pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
-    respostas: [
-      "Exibir uma mensagem de erro",
-      "Imprimir dados no console",
-      "Criar uma variável"
+    pergunta: "Qual clube venceu a última edição da UEFA Champions League?",
+    resposta: [
+      "Real Madrid",
+      "Liverpool",
+      "Bayern de Munique",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual jogador recebeu o prêmio de Melhor Jogador da UEFA em 2021?",
+    resposta: [
+      "Lionel Messi",
+      "Cristiano Ronaldo",
+      "Robert Lewandowski",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Em que país sediará a próxima Copa do Mundo da FIFA em 2022?",
+    resposta: [
+      "França",
+      "Alemanha",
+      "Catar",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual seleção nacional ganhou a Euro 2020?",
+    resposta: [
+      "Itália",
+      "Inglaterra",
+      "Espanha",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem é o atual artilheiro da Premier League?",
+    resposta: [
+      "Mohamed Salah",
+      "Harry Kane",
+      "Bruno Fernandes",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual clube venceu a última edição da La Liga espanhola?",
+    resposta: [
+      "Barcelona",
+      "Real Madrid",
+      "Atlético de Madrid",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Em que ano a França sediou e venceu a Copa do Mundo da FIFA?",
+    resposta: [
+      "1998",
+      "2002",
+      "2010",
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Quem foi o artilheiro da UEFA Champions League na temporada 2020-2021?",
+    resposta: [
+      "Erling Haaland",
+      "Kylian Mbappé",
+      "Robert Lewandowski",
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual país é conhecido por sua 'Série A' no futebol?",
+    resposta: [
+      "Espanha",
+      "Itália",
+      "Alemanha",
     ],
     correta: 1
   },
   {
-    pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-    respostas: [
-      "Comparação de valores sem considerar o tipo",
-      "Atribuição de valores",
-      "Comparação estrita de valores e tipos"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Como se declara uma variável em JavaScript?",
-    respostas: [
-      "let myVar;",
-      "const myVar = 10;",
-      "ambas as opções acima estão corretas"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "O que é uma função em JavaScript?",
-    respostas: [
-      "Um tipo de dado",
-      "Um bloco de código reutilizável",
-      "Uma variável global"
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
-    respostas: [
-      "Nenhuma, são sinônimos",
-      "let é usado para valores constantes, const para variáveis",
-      "let permite reatribuição, const cria variáveis imutáveis"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "O que é o DOM em JavaScript?",
-    respostas: [
-      "Um método de criptografia",
-      "Um modelo de objeto para manipular documentos HTML",
-      "Uma linguagem de programação"
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Como se realiza uma iteração sobre os elementos de um array em JavaScript?",
-    respostas: [
-      "Usando a estrutura 'if-else'",
-      "Com a declaração 'switch'",
-      "Utilizando loops como 'for' ou 'forEach'"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "O que é o JSON em JavaScript?",
-    respostas: [
-      "Um método de formatação de texto",
-      "Uma linguagem de estilização",
-      "Um formato de dados leve e intercambiável"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
-    respostas: [
-      "São iguais, usados de forma intercambiável",
-      "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-      "Ambos representam valores vazios"
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Como se adiciona um evento a um elemento HTML usando JavaScript?",
-    respostas: [
-      "Apenas com CSS",
-      "Usando o atributo 'event'",
-      "Através do método 'addEventListener'"
+    pergunta: "Qual jogador português é frequentemente comparado a Cristiano Ronaldo?",
+    resposta: [
+      "Bruno Fernandes",
+      "João Cancelo",
+      "Diogo Jota",
     ],
     correta: 2
   },
